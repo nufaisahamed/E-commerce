605a889df312ce3bb33bef99137395cb5b431e4f
nufaisahamed <ahamednufais@4161@gmail.com>
2025-05-17 11:31:13 +0530

    latest update

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,6 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports.verifyToken = (req, res, next) => {
     const token = req.cookies.authToken;
 
+    console.log(token);
+
     if (!token) {
         return res.status(401).json({ message: "No token provide!" });
     }
